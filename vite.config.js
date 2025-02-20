b8c1d467cbe9c3e1bad8a44b59cdbedd547cbd2b
rahul-0000-code <rahulkbhandari272002@gmail.com>
2025-02-20 22:36:16 +0530

    changed to tsx config repo

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths"; // Ensure this is installed
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  optimizeDeps: {
+    include: ['@emotion/react', '@emotion/styled', '@mui/material/Tooltip'],
+  },
+  plugins: [tsconfigPaths(), react()], // Remove tailwindcss plugin if not needed
+  preview: {
+    port: 5000,
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
 });
